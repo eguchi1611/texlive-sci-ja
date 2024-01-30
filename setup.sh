95cf0b7fc65a02395ca7776c31a77a3e553333f6
Keita Ito <eguchi@eguchi.cc>
2024-01-30 19:09:37 +0900

    サンプルの修正

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -fsSL https://github.com/eguchi1611/texlive-ja/archive/HEAD.tar.gz | tar -x --strip=1 */examples/devcontainer-basic
+curl -fsSL https://github.com/eguchi1611/texlive-ja/archive/HEAD.tar.gz | tar -x --strip=3 */examples/devcontainer-basic
